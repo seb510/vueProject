ba6a7715b7355c82dcb71030bc46d1cae34fe6d1
seb510 <sebistian@ukr.net>
2022-07-09 18:26:12 +0300

    fadded new v-bind

--- app2.js
+++ app2.js
@@ -5,7 +5,29 @@ Vue.createApp({
     //}
     data: () => ({
         myHtml: '<h1>Vue 3 App</h1>',
-        title: 'Я есто Грут'
-    })
+        title: 'Я есто Грут',
+        person: {
+            firstName: 'Nikolay',
+            lastName: "Sebistian",
+            age: 29
+        },
+        items: [1, 2]
+    }),
+    methods: {
+        addItem() {
+            this.items.unshift(this.$refs.myInput.value)
+        },
+        remove(i) {
+            this.items.splice(i, 1)
+        },
+        log(item) {
+            console.log('item', item)
+        }
+    },
+    computed: {
+        evenItems() {
+            return this.items.filter(i => i % 2 === 0)
+        }
+    }
 
 }).mount('#app')
